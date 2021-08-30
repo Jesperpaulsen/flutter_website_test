1e37b0be1f65466e7a6affdbe6e90ef0489e8124
Jesper Paulsen <jesper@jesper.no>
2021-08-30 20:55:03 +0200

    try new deploy

--- lib/services/capitalize.dart
+++
@@ -1,8 +0,0 @@
-extension Capitalize on String {
-  String capitalize() {
-    if (this.isEmpty) return this;
-    if (this.length == 1) return this.toUpperCase();
-    final firstChar = this[0].toUpperCase();
-    return '$firstChar${this.substring(1).toLowerCase()}';
-  }
-}
